37f5e9dc1755c5ca3fd60554bc9b84fb863461be
dujinqiao <duzi1996@qq.com>
2016-09-25 22:36:17 +0800

    modify template of java class

--- src/inherit/common/Role.java
+++ src/inherit/common/Role.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package inherit.common;
 
-/**
- *
- * @author dujinqiao
- */
 public abstract class Role {
 
     String name;
