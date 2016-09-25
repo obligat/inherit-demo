b39327b9496406f66755e929866a4a4e8d3224ee
dujinqiao <duzi1996@qq.com>
2016-09-25 19:32:23 +0800

    polymorphism

--- src/inherit/common/RPG.java
+++ src/inherit/common/RPG.java
@@ -12,29 +12,23 @@ package inherit.common;
 public class RPG {
 
     public static void main(String[] args) {
-        demoSwordsMan();
-        demoMagician();
-    }
-
-    static void demoSwordsMan() {
-        Role swordsMan = new SwordsMan();
-//    wrong    Magician magician = (Magician) swordsMan;
+        SwordsMan swordsMan = new SwordsMan();
         swordsMan.setName("Lici");
         swordsMan.setBlood(200);
         swordsMan.setLevel(1);
-        System.out.printf("swordsMan : %s , %d , %d %n", swordsMan.getName(), swordsMan.getBlood(), swordsMan.getLevel());
-    }
 
-    static void demoMagician() {
         Magician magician = new Magician();
-//Exception in thread "main" java.lang.ClassCastException: inherit.common.Role cannot be cast to inherit.common.Magician
-// Magician magician =  new Role();
-//Magician magician =  (Magician) new Role(); 
-        Role role = magician;
         magician.setName("Huba");
         magician.setBlood(100);
         magician.setLevel(1);
-        System.out.printf("magician : %s , %d , %d %n", magician.getName(), magician.getBlood(), magician.getLevel());
+        
+        showBlood(swordsMan);
+        showBlood(magician);
+
+    }
+
+    static void showBlood(Role role) {
+        System.out.printf("%s blood :%d %n", role.getName(), role.getBlood());
     }
 
 }
