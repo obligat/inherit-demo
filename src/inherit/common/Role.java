06642c1b643720c45ec8279101f054dd70013583
McDu <>
2016-09-25 10:12:55 +0800

    inital father class

---
+++ src/inherit/common/Role.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inherit.common;
+
+/**
+ *
+ * @author dujinqiao
+ */
+public class Role {
+
+    private String name;
+    private int level;
+    private int blood;
+
+    public int getBlood() {
+        return blood;
+    }
+
+    public void setBlood(int blood) {
+        this.blood = blood;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
